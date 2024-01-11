e2b52c374ed6cb5a05af32053eb2902780e79b94
devlrakib <devrakib5@gmail.com>
2024-01-11 20:13:20 +0600

    first commit

---
+++ index1.js
@@ -0,0 +1,27 @@
+var num = 20;
+// Number Type To String Type Convert rules;
+num =toString(num);
+console.log(typeof(num));
+
+// String Type To parseInt Conver System (Purno Sonkhar Somoy ParseInt Use Korte Hoi);
+var num1 ="30";
+num1=parseInt(num1)
+console.log(typeof(num1))
+
+// String Type To parseFloat Convert System (Doshomik Sonkar Somoy ParseFloat Use Korte Hoi);
+var num2 ="50.25";
+num2=parseFloat(num2)
+console.log(typeof(num2))
+
+// toFixed Number Type rules1 ;
+var number = 2.5678;
+console.log(number.toFixed())
+// toFixed Number Type rules2 ;
+var number = 2.5893;
+console.log(number.toFixed(1))
+
+
+//toPrecision Number Type (Total Doshomik Number Theke Akta Number Ber Korar System);
+var number =2.5789;
+console.log(number.toPrecision(1))
+
